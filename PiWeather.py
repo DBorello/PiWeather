f4ec75ade920fd24aff67a114776531cd17b8f10
Dan <dan@borellos.com>
2015-12-02 16:51:01 -0800

    Added requests

--- PiWeather.py
+++ PiWeather.py
@@ -1,1 +1,12 @@
 import time
+import requests
+
+class WUnderground():
+	def __init__(self):
+		pass
+
+	def GetLocal(self):
+
+
+
+Weather = WUnderground()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PiWeather
 Analog weather station powered by a Raspberry Pi
+
+## Dependencies
+* python-requests
