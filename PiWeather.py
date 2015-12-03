aa22a2843352f1591dfe850f335309a2263c8c90
Dan <dan@borellos.com>
2015-12-02 17:20:02 -0800

    Syntax

--- PiWeather.py
+++ PiWeather.py
@@ -13,14 +13,14 @@ class WUnderground():
 	def GetLocal(self):
 		r = requests.get('http://api.wunderground.com/api/d567171cf9081060/geolookup/q/autoip.json')
 		data = r.json()
-		logging.debug('Raw geoip response',data)
+		logging.debug('Raw geoip response: %s',str(data))
 		self.Station = data['location']['nearby_weather_stations']['pws']['station'][0]['id']
 		logging.info('Using geolocation station: ',self.Station)
 
 	def GetWeather(self):
 		r = requests.get('http://stationdata.wunderground.com/cgi-bin/stationlookup?station={0:s}&units=english&v=2.0&format=json&_={1:d}'.format(self.Station,int(round(time.time()*1000,0))))
 		data = r.json()
-		logging.debug('Raw weather response',data)
+		logging.debug('Raw weather response: %s',str(data))
 
 		StationData = data['stations'][list(data['stations'].keys())[0]]
 
@@ -30,7 +30,7 @@ class WUnderground():
 		self.Current['Pressure'] = StationData['pressure']
 		self.Current['Wind'] =  StationData['wind_speed']
 
-		logging.info('Recieved weather: ',str(self.Current))
+		logging.info('Recieved weather: %s ',str(self.Current))
 
 		return self.Current
 
