7c52db14cb4a0aee8c4b649003f1f6a49dabbb44
Dan <dan@borellos.com>
2015-12-02 17:05:03 -0800

    Added GetWeather

--- PiWeather.py
+++ PiWeather.py
@@ -3,12 +3,18 @@ import requests
 
 class WUnderground():
 	def __init__(self):
-		pass
+		self.Station = ''
+		self.GetLocal()
 
 	def GetLocal(self):
 		r = requests.get('http://api.wunderground.com/api/d567171cf9081060/geolookup/q/autoip.json')
 		data = r.json()
-		print(data)
+		self.Station = data['location']['nearby_weather_stations']['pws'][0]['id']
+
+	def GetWeather(self):
+		r = requests.get('http://stationdata.wunderground.com/cgi-bin/stationlookup?station={0:s}&units=english&v=2.0&format=json&_={1:i}'.format(self.Station,round(time.time()*1000,0)))
+		data = r.json
+		print(r.json)
 
 Weather = WUnderground()
-Weather.GetLocal()
+Weather.GetWeather()
