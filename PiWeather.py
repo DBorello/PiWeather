e0d957791b7519c121df96f0350f23b82606b403
Dan <dan@borellos.com>
2015-12-02 17:08:13 -0800

    Syntax

--- PiWeather.py
+++ PiWeather.py
@@ -17,7 +17,7 @@ class WUnderground():
 	def GetWeather(self):
 		r = requests.get('http://stationdata.wunderground.com/cgi-bin/stationlookup?station={0:s}&units=english&v=2.0&format=json&_={1:d}'.format(self.Station,int(round(time.time()*1000,0))))
 		data = r.json()
-		print(r.json)
+		print(data)
 
 Weather = WUnderground()
 Weather.GetWeather()
